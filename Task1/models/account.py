4d02095f80a9a0bf80dff8e330d6c8cd7e81f294
King Kwan <kwankingting@outlook.com>
2026-03-06 00:05:18 +0800

    Refactor account class for English terminology

--- Task1/models/account.py
+++ Task1/models/account.py
@@ -9,7 +9,7 @@ class Account:
     def get_balance(self) -> float:
         total = 0.0
         for t in self._transactions:
-            if t.get_type() == "收入":
+            if t.get_type() == "Income":  
                 total += t._amount
             else:
                 total -= t._amount
@@ -19,4 +19,4 @@ class Account:
         return self._transactions
 
     def __str__(self):
-        return f"帳戶: {self._name} | 結餘: ${self.get_balance():.2f}"
+        return f"Account: {self._name} | Balance: ${self.get_balance():.2f}"
